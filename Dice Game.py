eb67686fae9f838d5a89143bb23c578a8594aa70
Ravindu Aluvihare <155241077+ravindu-aluvihare@users.noreply.github.com>
2025-02-02 15:11:48 +0530

    Delete Dice Game.py

--- Dice Game.py
+++
@@ -1,9 +0,0 @@
-import random
-
-again = "y"
-
-while again == "y":
-    print("Rolling the dice...")
-    print("The value is: ", random.randint(1, 6))
-
-    again = input("Roll the dice again? (y/n): ")
